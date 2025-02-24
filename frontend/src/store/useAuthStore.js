2b0002679d912742b1dea66c021b975dcb945293
EQKNF <emil.fredriksen@hotmail.no>
2025-02-24 18:14:01 +0100

    add Navbar component with authentication links and logout functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -32,5 +32,28 @@ export const useAuthStore = create((set) => ({
             set({ isSigningUp: false });
         }
     },
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post('/auth/login', data);
+            toast.success("Logged in successfully");
+            set({ authUser: res.data});
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }finally{
+            set({ isLoggingIn: false });
+        }
+    },
+    logout: async () => {
+        try {
+            await axiosInstance.post('/auth/logout');
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    },
+
+
 })  
 ); 

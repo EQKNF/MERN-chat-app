376e174804b452b3f31f17f81947ff03a4f21c35
EQKNF <emil.fredriksen@hotmail.no>
2025-02-26 19:00:51 +0100

    feat: implement profile picture upload and display on ProfilePage

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -53,6 +53,18 @@ export const useAuthStore = create((set) => ({
             toast.error(error.response.data.message);
         }
     },
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put('/auth/update-profile', data);
+            toast.success("Profile updated successfully");
+            set({ authUser: res.data});
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }finally{
+            set({ isUpdatingProfile: false });
+        }
+    },
 
 
 })  

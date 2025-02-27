b15c455a74b9b50f85ae808114f5c395e17db357
EQKNF <emil.fredriksen@hotmail.no>
2025-02-27 21:08:52 +0100

    feat: implement chat functionality with Sidebar, ChatContainer, and NoChatSelected components

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useChatStore } from '../store/useChatStore'
+
+const Sidebar = () => {
+    const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
+    return (
+        <div>Sidebar</div>
+    )
+}
+
+export default Sidebar

e2183d5acf5d5b6b46b1f5e47272abedfdd55b92
EQKNF <emil.fredriksen@hotmail.no>
2025-02-28 20:53:52 +0100

    refactor(message.controller): update message query to use senderId and receiverId fields

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -17,22 +17,22 @@ export const getUsersForSidebar = async (req, res) => {
 
 export const getMessages = async (req, res) => {
     try {
-        const {id:userToChatId} = req.params;
-        const myId = req.user._id;
-
-        const messages = await Message.find({
-            $or: [
-                { sender: myId, receiver: userToChatId },
-                { sender: userToChatId, receiver: myId }
-            ]
-        })
-
-        res.status(200).json(messages);
+      const { id: userToChatId } = req.params;
+      const myId = req.user._id;
+  
+      const messages = await Message.find({
+        $or: [
+          { senderId: myId, receiverId: userToChatId },
+          { senderId: userToChatId, receiverId: myId },
+        ],
+      });
+  
+      res.status(200).json(messages);
     } catch (error) {
-        console.error("Error in message controller: ", error.message);
-        res.status(500).json({ error: "internal server error" });
+      console.log("Error in getMessages controller: ", error.message);
+      res.status(500).json({ error: "Internal server error" });
     }
-}
+  };
 
 export const sendMessage = async (req, res) => {
     try {

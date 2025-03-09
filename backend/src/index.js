b5296b80df6c780697e41c8300e0843db9f53f04
EQKNF <emil.fredriksen@hotmail.no>
2025-03-09 23:33:11 +0100

    fix: standardize import statements and improve formatting in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,15 @@
-import dotenv from 'dotenv';
 import express from "express";
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import path from 'path';
+import path from "path";
 
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import { connectDB } from './lib/db.js';
-import {app, server} from './lib/socket.js';
+import { connectDB } from "./lib/db.js";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
@@ -17,25 +17,24 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}));
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-
-app.use(express.static(path.join(__dirname, "../../frontend", "dist")));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
-    console.log('Server is running on PORT:' + PORT);
-    connectDB();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });

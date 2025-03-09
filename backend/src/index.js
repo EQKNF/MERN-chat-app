60c26ec4e8a564405b24bbf766b36392a4a2a7a2
EQKNF <emil.fredriksen@hotmail.no>
2025-03-10 00:59:55 +0100

    fix: update static file serving paths in index.js for correct directory structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use(express.json());
@@ -28,10 +28,10 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 server.listen(PORT, () => {
